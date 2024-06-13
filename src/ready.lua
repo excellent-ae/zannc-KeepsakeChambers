109f57472f2d38cb2944072828bc2716349e2037
x00179520 <xspelz@gmail.com>
2024-06-13 07:45:05 +0100

    Closes #1

--- src/ready.lua
+++ src/ready.lua
@@ -2,15 +2,20 @@
 ---@diagnostic disable: lowercase-global
 
 modutil.mod.Path.Context.Wrap("AdvanceKeepsake", function()
-    modutil.mod.Path.Wrap("IncrementTableValue", function(base, tableArg, key, amount)
-        if amount == nil then
-            amount = config.Increment
-        end
+    local traitName = game.GameState.LastAwardTrait
+    local startingKeepsakeLevel = GetKeepsakeLevel(traitName, true)
 
-        if tableArg[key] == nil then
-            tableArg[key] = 0
-        end
+    if startingKeepsakeLevel < 3 then
+        modutil.mod.Path.Wrap("IncrementTableValue", function(base, tableArg, key, amount)
+            if amount == nil then
+                amount = config.Increment
+            end
 
-        tableArg[key] = tableArg[key] + amount
-    end)
+            if tableArg[key] == nil then
+                tableArg[key] = 0
+            end
+
+            tableArg[key] = tableArg[key] + amount
+        end)
+    end
 end)
